df763cab0314076da17187d008c48a950027b53d
Sunil Kr. Singh <sunil.singh@economical.com>
2022-05-21 22:54:18 -0400

    cleaning up

--- starter/app/src/main/java/com/udacity/shoestore/models/User.kt
+++
@@ -1,7 +0,0 @@
-package com.udacity.shoestore.models
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class User(var userName: String, var password: String) : Parcelable
